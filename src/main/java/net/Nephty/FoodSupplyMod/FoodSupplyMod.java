b2018bddb0c5b8efb824e3a482e8c76b6646cedf
Nephty <211260@umons.ac.be>
2022-02-09 18:59:43 +0100

    added fruit resources

--- src/main/java/net/Nephty/FoodSupplyMod/FoodSupplyMod.java
+++ src/main/java/net/Nephty/FoodSupplyMod/FoodSupplyMod.java
@@ -1,5 +1,6 @@
 package net.Nephty.FoodSupplyMod;
 
+import net.Nephty.FoodSupplyMod.item.ModItem;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,13 +9,16 @@ public class FoodSupplyMod implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LogManager.getLogger("foodsupply");
+    public static final String MODID = "foodsupply";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
 
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
+
+        ModItem.registerModItems();
 
         LOGGER.info("Hello Fabric world!");
     }
